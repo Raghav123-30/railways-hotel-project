de348ab6ce748e56e623d3893f7ff13cd85b40e2
Raghav123-30 <Raghavendrabhat2018@gmail.com>
2024-02-03 00:38:04 +0530

    implemented error handling when table data goes null

--- src/components/rooms/roomsTable.tsx
+++ src/components/rooms/roomsTable.tsx
@@ -14,32 +14,36 @@ type roomsTableProps = {
   rooms: room[];
 };
 const RoomsTable = ({ rooms }: roomsTableProps) => {
-  return (
-    <Table>
-      <TableCaption>A list of rooms.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Room number</TableHead>
-          <TableHead>Availability</TableHead>
-          <TableHead>Cleaning status</TableHead>
-          <TableHead>Price</TableHead>
-          <TableHead>Bed type</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {rooms.map((room) => (
-          <TableRow key={room.roomNumber}>
-            <TableHead>{room.roomNumber}</TableHead>
-            <TableHead>{room.availability}</TableHead>
-            <TableHead>{room.cleaningStatus}</TableHead>
-            <TableHead>{room.price}</TableHead>
-            <TableHead>{room.bedType}</TableHead>
+  if (rooms) {
+    return (
+      <Table>
+        <TableCaption>A list of rooms.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Room number</TableHead>
+            <TableHead>Availability</TableHead>
+            <TableHead>Cleaning status</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Bed type</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter></TableFooter>
-    </Table>
-  );
+        </TableHeader>
+        <TableBody>
+          {rooms.map((room) => (
+            <TableRow key={room.roomNumber}>
+              <TableHead>{room.roomNumber}</TableHead>
+              <TableHead>{room.availability}</TableHead>
+              <TableHead>{room.cleaningStatus}</TableHead>
+              <TableHead>{room.price}</TableHead>
+              <TableHead>{room.bedType}</TableHead>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter></TableFooter>
+      </Table>
+    );
+  } else {
+    return <p>Data could not be loaded</p>;
+  }
 };
 
 export default RoomsTable;
